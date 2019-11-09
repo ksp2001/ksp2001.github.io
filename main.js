0d38536f42e4295f46e662d00b24849a74ab976e
Ketki Panse <ketki.panse@shopify.com>
2019-11-08 21:58:46 -0500

    styling changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* for the particles background */
 particlesJS.load('particles-js', 'particles.json')
-var typed = new Typed('#typed', {
-  stringsElement: '#typed-strings ',
+var typed = new Typed('.typed', {
+  stringsElement: '.typed-strings ',
   typeSpeed : 30,
   loop: !0,
   backDelay: 2000,
