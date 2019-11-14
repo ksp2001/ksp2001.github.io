e747e7fd2ecb82ad3e1b0d2bb1693e21fed97fee
Ketki Panse <ketki.panse@shopify.com>
2019-11-14 17:44:14 -0500

    Final version

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 /* for the particles background */
 particlesJS.load('particles-js', 'particles.json')
+particlesJS.load('particles-js-2', 'particles.json')
 var typed = new Typed('.typed', {
   stringsElement: '.typed-strings ',
   typeSpeed : 30,
   loop: !0,
   backDelay: 2000,
     });
+
+    document.querySelectorAll('.scroll-link').forEach(a => {
+      /* e is a mouse event */
+      a.addEventListener('click', function(e) {
+          e.preventDefault();
+  
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+
