56c47094749db7c5920ffdbe75003d28eb7ab280
Ketki Panse <ketki.panse@shopify.com>
2019-11-15 09:46:12 -0500

    Adding all files

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 /* for the particles background */
 particlesJS.load('particles-js', 'particles.json')
 particlesJS.load('particles-js-2', 'particles.json')
-var typed = new Typed('.typed', {
+const typed = new Typed('.typed', {
   stringsElement: '.typed-strings ',
   typeSpeed : 30,
   loop: !0,
   backDelay: 2000,
     });
 
+
+    // assignment requirement complete by implementing a smooth
+    // scroll for my links
     document.querySelectorAll('.scroll-link').forEach(a => {
       /* e is a mouse event */
       a.addEventListener('click', function(e) {
@@ -19,3 +22,26 @@ var typed = new Typed('.typed', {
       });
   });
 
+
+  // making a back to top button which appears after user has scrolled
+  // past the first screen, which takes them back to the top
+  mybutton = document.querySelector(".my-button");
+
+  // When the user scrolls down 70px from the top of the document, show the button
+  window.onscroll = () => {scrollFunction()};
+  
+  function scrollFunction() {
+    /* make it fit for all browsers hence the || :: this means if
+    user scrolls over 50px, this button will start showing*/
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
